2076812a6b59fa9ca96d8ebba0bb4e6264b5866b
AfonsoAlemao <afonso.alemao01@gmail.com>
2023-11-14 21:20:56 +0000

    ex1

---
+++ Lab_1/exercise_1/ex1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 500
+
+int main(int argc, char * argv[]){
+
+    char *result_str;
+
+    result_str = (char *)malloc(MAX * sizeof(char));
+
+    for (int i = 1; i < argc; i++) {
+        if(i == 1) {
+            strncpy(result_str, argv[i], strlen(argv[i]));
+        } else {
+            strncat(result_str, argv[i], strlen(argv[i]));
+        }
+    }
+
+
+    printf("result_str:  %s\n", result_str);   
+    exit(0);
+}
