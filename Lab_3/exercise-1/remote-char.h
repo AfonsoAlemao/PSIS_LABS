23ec65e51380bd3b317db79e0cc4a1603a5f7c01
AfonsoAlemao <afonso.alemao01@gmail.com>
2023-11-18 17:58:57 +0000

    TODO 1/ TODO 2

--- Lab_3/exercise-1/remote-char.h
+++ Lab_3/exercise-1/remote-char.h
@@ -1,5 +1,20 @@
 // TODO_1 
 // declaration the struct corresponding to the exchanged messages
 
+typedef enum direction_t {UP, DOWN, LEFT, RIGHT} direction_t;
+
+typedef struct connection {
+    int ch; // character selected by the user
+} connection;
+
+
+typedef struct movement {
+    int ch; // character selected by the user
+    direction_t diretion;
+} movement;
+
+
 // TODO_2
 //declaration of the FIFO location
+
+#define FIFO_NAME "/tmp/fifo_server"
